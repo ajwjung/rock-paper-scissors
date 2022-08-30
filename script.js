34f8f66ddddfd2054ce40f4fa64508a5e08f8a18
Amy Jung <ajwjung@outlook.com>
2022-08-30 00:39:40 -0700

    Create a function to play a round

--- script.js
+++ script.js
@@ -9,10 +9,38 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
-// Write function to play a single round
-// Function takes two parameters: player selection and computer selection
-    // Must be case insensitive ^^
-// Return a statement that declares the winner (NOT console.log())
+// Function: play a single round
+// Requires computer's selection, user's selection
+// Compare computer vs. user using if/else statements
+// Return string to declare winner of the round
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = prompt(`Make a selection: "Rock", "Paper", or "Scissors"`).toLowerCase();
+
+    console.log(computerSelection, playerSelection);
+
+    let playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
+    let computerWins = `You lose! ${computerSelection} beats ${playerSelection}`;
+    let draw = `It's a tie! Both selected ${playerSelection}`
+    let winningMessage;
+    
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        winningMessage = playerWins;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        winningMessage = playerWins;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        winningMessage = playerWins;
+    } else if (playerSelection == computerSelection) {
+        winningMessage = draw;
+    } else {
+        winningMessage = computerWins;
+    }
+    
+    return winningMessage;
+}
+
+console.log(playRound());
+
 
 // Write function that plays 5 rounds using the function above ^^
     // Use console.log() to display who the winner is after each round
