04c5a1b00dae49c8b53c66e6d7902c92eaa30fe4
Amy Jung <ajwjung@outlook.com>
2022-08-30 01:41:22 -0700

    Update comments and remove unwanted code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Function: play against computer
-// Computer randomly returns "rock" "paper" or "scissors"
 function getComputerChoice() {
     const allChoices = ["Rock", "Paper", "Scissors"];
     let selection = Math.floor(Math.random() * allChoices.length)
@@ -7,17 +5,10 @@ function getComputerChoice() {
     return allChoices[selection];
 }
 
-// console.log(getComputerChoice());
-
-// Function: play a single round
-// Requires computer's selection, user's selection
-// Compare computer vs. user using if/else statements
-// Return string to declare winner of the round
+// Plays one round
 function playRound(playerSelection, computerSelection) {
     computerSelection = getComputerChoice().toLowerCase();
     playerSelection = prompt(`Make a selection: "Rock", "Paper", or "Scissors"`).toLowerCase();
-
-    // console.log(computerSelection, playerSelection);
 
     let playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
     let computerWins = `You lose! ${computerSelection} beats ${playerSelection}`;
@@ -44,13 +35,7 @@ function playRound(playerSelection, computerSelection) {
     return thisRoundWinner;
 }
 
-// console.log(playRound());
-
-
-// Function: play 5 rounds using a loop and playRound()
-// Use if/else statements to tally player and computer scores each round
-// Compare scores using if/else and print the final winner to console
-
+// Plays game five times
 function game() {
     let playerScore = 0;
     let computerScore = 0;
